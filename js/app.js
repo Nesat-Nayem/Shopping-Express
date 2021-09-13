aa3c6bbe517a5a9daada417a45cbfe8ab19ff5ef
Nesat-Nayem <nesatnayem042@gmail.com>
2021-09-13 19:56:23 +0600

    formating code

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ const loadProducts = () => {
 };
 loadProducts();
 
-// show all product in UI 
+//`` show all product in UI  ``
+
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
@@ -30,16 +31,17 @@ const showProducts = (products) => {
     document.getElementById("all-products").appendChild(div);
   }
 };
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
-
   updateTotal()
   document.getElementById("total-Products").innerText = count;
 };
+
+//```````  modal fetch  ```
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
@@ -47,73 +49,52 @@ const getInputValue = (id) => {
   return converted;
 };
 
-
-
-// face agin me
-
-// const newii = addToCart(`${product.id}`)
-// console.log(newii);
-
-
 const loadProducts2 = (id) => {
-
   const url = `https://fakestoreapi.com/products/${id}`;
   fetch(url)
-          //  console.log(url);
     .then((response1) => response1.json())
-   
     .then((data1) => modalOpener(data1));
-    //  .then((data1) => console.log(data1.products[1]));
-    //  .then((data1) => console.log(data1));
+    
 };
+
+//  ````  model body   ``````
 
 const modalOpener = info =>{
   console.log(info);
-  for(const kika in info)
-  console.log(`info.${kika} = ${info[kika]}`);
-
-
+  for(const modalinfo in info)
+  console.log(`info.${modalinfo} = ${info[modalinfo]}`);
 const modalInformation = document.getElementById('modalData');
-
 modalInformation.innerHTML = `
-
-
-<div class="modal-header">
-<h5 class="modal-title" id="staticBackdropLabel">Title: ${info.title} </h5>
-<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-</div>
-<div class="modal-body"> Description: 
-<p>${info.description}</p>
-</div>
-<div class="modal-footer">
-<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-<button type="button" class="btn btn-primary">Understood</button>
-</div>
-
-`
+  <div class="modal-header">
+  <h5 class="modal-title" id="staticBackdropLabel">Title: ${info.title} </h5>
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  <div class="modal-body"> Description: 
+  <p>${info.description}</p>
+  </div>
+  <div class="modal-footer">
+  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+  <button type="button" class="btn btn-primary">Understood</button>
+  </div>`
 }
-// modal body
 
+//``` main price update function   ```//
 
-
-
-
-
-
-// main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = total.toFixed(2);
+  document.getElementById(id).innerText = (total).toFixed(2);
 };
 
-// set innerText function
+//`` set innerText function ``\\
+
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = Math.round(value);
 };
 
-// update delivery charge and total Tax
+//**     update delivery charge and total Tax     **//
+
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
   if (priceConverted > 200) {
@@ -130,10 +111,11 @@ const updateTaxAndCharge = () => {
   }
 };
 
-//grandTotal update function
+//**   grandTotal update function    **\\
+
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  getInputValue("price") + getInputValue("delivery-charge")+
+   getInputValue("total-tax");
+  document.getElementById("total").innerText =grandTotal.toFixed(2);
 };
